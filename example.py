fb1335c64def9035cc33139973b4ee3f91c62e6e
SoftLayer <sldn@softlayer.com>
2010-06-28 11:35:28 -0500

    Added interpreter and encoding lines to the example script. (Thanks, mahmoudimus!)

--- example.py
+++ example.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
 # Copyright (c) 2010, SoftLayer Technologies, Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
