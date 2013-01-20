621932ec916e345fb29c5fa6e810ba94e2118d12
Kevin McDonald <k3vinmcdonald@gmail.com>
2013-01-19 20:17:48 -0600

    Updates Copyright Year

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2010, SoftLayer Technologies, Inc. All rights reserved.
+# Copyright (c) 2013, SoftLayer Technologies, Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
