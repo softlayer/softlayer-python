494c62f729c6aad4a13c5f4942cfd774039104b8
Kevin McDonald <k3vinmcdonald@gmail.com>
2013-02-14 20:36:03 -0600

    Adds MetadataManager

--- SoftLayer/consts.py
+++ SoftLayer/consts.py
@@ -3,4 +3,5 @@
 VERSION = '2.1.0'
 API_PUBLIC_ENDPOINT = 'https://api.softlayer.com/xmlrpc/v3/'
 API_PRIVATE_ENDPOINT = 'https://api.service.softlayer.com/xmlrpc/v3/'
+API_PRIVATE_ENDPOINT_REST = 'https://api.service.softlayer.com/rest/v3/'
 USER_AGENT = "SoftLayer Python %s" % VERSION
--- SoftLayer/tests/API/client_tests.py
+++ SoftLayer/tests/API/client_tests.py
@@ -1,7 +1,7 @@
 try:
     import unittest2 as unittest
 except ImportError:
-    import unittest
+    import unittest # NOQA
 
 from mock import patch, MagicMock
 
