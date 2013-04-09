a9732c21ffac4398be213068a4c3caba415bc4ec
Kevin McDonald <k3vinmcdonald@gmail.com>
2013-04-09 15:29:52 -0500

    Version Bump. Adds CHANGELOG

--- SoftLayer/consts.py
+++ SoftLayer/consts.py
@@ -6,7 +6,7 @@
     :copyright: (c) 2013, SoftLayer Technologies, Inc. All rights reserved.
     :license: BSD, see LICENSE for more details.
 """
-VERSION = 'v2.1.0'
+VERSION = 'v2.2.0'
 API_PUBLIC_ENDPOINT = 'https://api.softlayer.com/xmlrpc/v3/'
 API_PRIVATE_ENDPOINT = 'https://api.service.softlayer.com/xmlrpc/v3/'
 API_PRIVATE_ENDPOINT_REST = 'https://api.service.softlayer.com/rest/v3/'
