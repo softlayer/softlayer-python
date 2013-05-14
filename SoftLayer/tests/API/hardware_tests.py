980b25dd527288d0ac4d5efc650ba772c974cfb2
Brian Cline <bcline@softlayer.com>
2013-05-14 17:14:48 -0500

    Fixes name of hardware manager in tests

--- SoftLayer/tests/API/hardware_tests.py
+++ SoftLayer/tests/API/hardware_tests.py
@@ -1,5 +1,5 @@
 """
-    SoftLayer.tests.API.cci_tests
+    SoftLayer.tests.API.hardware_tests
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     :copyright: (c) 2013, SoftLayer Technologies, Inc. All rights reserved.
@@ -19,17 +19,17 @@ class HardwareTests_unittests(unittest.TestCase):
 
     def setUp(self):
         self.client = MagicMock()
-        self.cci = SoftLayer.HardwareManager(self.client)
+        self.hardware = SoftLayer.HardwareManager(self.client)
 
     def test_list_hardware(self):
         mcall = call(mask=ANY, filter={})
         service = self.client.__getitem__()
 
-        self.cci.list_hardware()
+        self.hardware.list_hardware()
         service.getHardware.assert_has_calls(mcall)
 
     def test_list_hardware_with_filters(self):
-        self.cci.list_hardware(
+        self.hardware.list_hardware(
             hostname='hostname',
             public_ip='1.2.3.4',
             private_ip='4.3.2.1',
@@ -48,30 +48,30 @@ class HardwareTests_unittests(unittest.TestCase):
 
     def test_resolve_ids_ip(self):
         self.client.__getitem__().getHardware.return_value = [{'id': '1234'}]
-        _id = self.cci._get_ids_from_ip('1.2.3.4')
+        _id = self.hardware._get_ids_from_ip('1.2.3.4')
         self.assertEqual(_id, ['1234'])
 
         self.client.__getitem__().getHardware.side_effect = \
             [[], [{'id': '4321'}]]
-        _id = self.cci._get_ids_from_ip('4.3.2.1')
+        _id = self.hardware._get_ids_from_ip('4.3.2.1')
         self.assertEqual(_id, ['4321'])
 
-        _id = self.cci._get_ids_from_ip('nope')
+        _id = self.hardware._get_ids_from_ip('nope')
         self.assertEqual(_id, [])
 
     def test_resolve_ids_hostname(self):
         self.client.__getitem__().getHardware.return_value = [{'id': '1234'}]
-        _id = self.cci._get_ids_from_hostname('hostname')
+        _id = self.hardware._get_ids_from_hostname('hostname')
         self.assertEqual(_id, ['1234'])
 
     def test_get_instance(self):
         self.client.__getitem__().getObject.return_value = {
             'hourlyVirtualGuests': "this is unique"}
-        self.cci.get_hardware(1)
+        self.hardware.get_hardware(1)
         self.client.__getitem__().getObject.assert_called_once_with(
             id=1, mask=ANY)
 
     def test_reload(self):
-        self.cci.reload(id=1)
+        self.hardware.reload(id=1)
         f = self.client.__getitem__().reloadCurrentOperatingSystemConfiguration
         f.assert_called_once_with(id=1, token='FORCE')
