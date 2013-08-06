5190fa1f792542e2a277e83d9656b4aafb71c39d
Nathan Beittenmiller <nbeittenmiller@softlayer.com>
2013-08-06 11:11:01 -0500

    Adding in subnet unit tests

--- SoftLayer/tests/managers/network_tests.py
+++ SoftLayer/tests/managers/network_tests.py
@@ -7,7 +7,6 @@
 """
 from SoftLayer import NetworkManager
 from SoftLayer.tests import unittest
-
 from mock import MagicMock, ANY, call
 
 
@@ -17,6 +16,14 @@ class NetworkTests(unittest.TestCase):
         self.client = MagicMock()
         self.network = NetworkManager(self.client)
 
+    def test_get_subnet(self):
+        id = 9876
+        mcall = call(id=id, mask=ANY)
+        service = self.client['Network_Subnet']
+
+        self.network.get_subnet(id)
+        service.getObject.assert_has_calls(mcall)
+
     def test_get_vlan(self):
         id = 1234
         mcall = call(id=id, mask=ANY)
@@ -24,6 +31,45 @@ class NetworkTests(unittest.TestCase):
 
         self.network.get_vlan(id)
         service.getObject.assert_has_calls(mcall)
+
+    def test_list_subnets_default(self):
+        mcall = call(filter={}, mask=ANY)
+        service = self.client['Account']
+
+        self.network.list_subnets()
+
+        service.getSubnets.assert_has_calls(mcall)
+
+    def test_list_subnets_with_filters(self):
+        identifier = '10.0.0.1'
+        datacenter = 'dal00'
+        version = 4
+
+        service = self.client['Account']
+        service.getSubnets.return_value = [
+            {
+                'id': 100,
+                'networkIdentifier': '10.0.0.1',
+                'datacenter': {'name': 'dal00'},
+                'version': 4,
+            },
+            {
+                'id': 101,
+                'networkIdentifier': '10.0.1.1',
+                'datacenter': {'name': 'dal05'},
+                'version': 4,
+            },
+        ]
+
+        result = self.network.list_subnets(
+            identifier=identifier,
+            datacenter=datacenter,
+            version=version,
+        )
+
+        service.getSubnets.assert_called()
+
+        self.assertEqual([service.getSubnets.return_value[0]], result)
 
     def test_list_vlans_default(self):
         mcall = call(filter={}, mask=ANY)
@@ -86,3 +132,26 @@ class NetworkTests(unittest.TestCase):
 
         service.getNetworkVlans.assert_has_calls(mcall)
         self.assertEqual(expected, result)
+
+    def test_resolve_ids_ip(self):
+        service = self.client['Account']
+        service.getSubnets.return_value = [
+            {
+                'id': '100',
+                'networkIdentifier': '10.0.0.1',
+                'datacenter': {'name': 'dal00'},
+                'version': 4,
+            },
+            {
+                'id': '101',
+                'networkIdentifier': '10.0.1.1',
+                'datacenter': {'name': 'dal05'},
+                'version': 4,
+            },
+        ]
+
+        _id = self.network._get_subnet_by_identifier('10.0.0.1')
+        self.assertEqual(_id, ['100'])
+
+        _id = self.network._get_subnet_by_identifier('nope')
+        self.assertEqual(_id, [])
