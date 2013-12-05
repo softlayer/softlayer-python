460e7daef19b5f6d314b009c087b0860c911cb80
Kevin McDonald <k3vinmcdonald@gmail.com>
2013-12-05 15:26:45 -0600

    Fixes typo in hardware create command

--- SoftLayer/CLI/modules/server.py
+++ SoftLayer/CLI/modules/server.py
@@ -706,10 +706,10 @@ Optional:
   -k KEY, --key=KEY    SSH keys to assign to the root user. Can be specified
                          multiple times.
   --dry-run, --test    Do not create the server, just get a quote
-  --vlan_public=VLAN   The ID of the public VLAN on which you want the CCI
-                         placed.
-  --vlan_private=VLAN  The ID of the private VLAN on which you want the CCI
-                         placed.
+  --vlan_public=VLAN   The ID of the public VLAN on which you want the hardware
+                         placed
+  --vlan_private=VLAN  The ID of the private VLAN on which you want the
+                         hardware placed
   -t, --template=FILE  A template file that defaults the command-line
                          options using the long name in INI format
   --export=FILE        Exports options to a template file
