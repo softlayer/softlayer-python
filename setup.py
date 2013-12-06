b81456456341727af5f54d0c24c108a3356e30b8
Nathan Beittenmiller <nbeittenmiller@softlayer.com>
2013-12-06 08:53:58 -0600

    Update version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ else:
 
 setup(
     name='SoftLayer',
-    version='3.0.1',
+    version='3.0.2',
     description=description,
     long_description=long_description,
     author='SoftLayer Technologies, Inc.',
