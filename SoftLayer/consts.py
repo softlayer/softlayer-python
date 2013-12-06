74ad06bf48ab96d4b579c454d6b9ac9c3262f611
Nathan Beittenmiller <nbeittenmiller@softlayer.com>
2013-12-06 08:56:41 -0600

    Updating version number

--- SoftLayer/consts.py
+++ SoftLayer/consts.py
@@ -6,7 +6,7 @@
     :copyright: (c) 2013, SoftLayer Technologies, Inc. All rights reserved.
     :license: MIT, see LICENSE for more details.
 """
-VERSION = 'v3.0.1'
+VERSION = 'v3.0.2'
 API_PUBLIC_ENDPOINT = 'https://api.softlayer.com/xmlrpc/v3/'
 API_PRIVATE_ENDPOINT = 'https://api.service.softlayer.com/xmlrpc/v3/'
 API_PUBLIC_ENDPOINT_REST = 'https://api.softlayer.com/rest/v3/'
