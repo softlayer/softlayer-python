4096e6550825f65d4b680dbc555045bdf688dfdb
anasouma <waelriac@us.ibm.com>
2014-04-11 10:03:07 -0700

    Fix test errors

--- SoftLayer/tests/fixtures/Network_Application_Delivery_Controller_LoadBalancer_VirtualIpAddress.py
+++ SoftLayer/tests/fixtures/Network_Application_Delivery_Controller_LoadBalancer_VirtualIpAddress.py
@@ -1,4 +1,4 @@
-getBillingItem = {'id': 21370814 }
+getBillingItem = {'id': 21370814}
 getObject = {
     'accountId': 307608,
     'connectionLimit': 500,
@@ -11,48 +11,47 @@ getObject = {
     'sslActiveFlag': False,
     'sslEnabledFlag': True,
     'virtualServers': [
-    {
-        'allocation': 10,
-        'id': 50718,
-        'port': 80,
-        "serviceGroups": [
-            {
-                'id': 51758,
-                'routingMethodId': 10,
-                'routingTypeId': 3,
-                'services': [
-                    {
-                        'enabled': 1,
-                        'id': 1234,
-                        'healthChecks': [
-                            {
-                                'id': 112112
-                            }
-                        ],
-                        'groupReferences': [
-                            {
-                                'serviceGroupId': 51758,
-                                'serviceId': 84986,
-                                'weight': 2
-                            }
-                        ],
-                        'ipAddressId': 14288108,
-                        'port': 8080,
-                        'status': "DOWN"
-                    }
-                ]
-            }
-        ],
-        "virtualIpAddress": {
-            'accountId': 307608,
-            'connectionLimit': 500,
-            'connectionLimitUnits': "connections/second",
-            'id': 22348,
-            'ipAddressId': 7303278,
-        },
-        'virtualIpAddressId': 22348
-    }
-]}
+        {
+            'allocation': 10,
+            'id': 50718,
+            'port': 80,
+            "serviceGroups": [
+                {
+                    'id': 51758,
+                    'routingMethodId': 10,
+                    'routingTypeId': 3,
+                    'services': [
+                        {
+                            'enabled': 1,
+                            'id': 1234,
+                            'healthChecks': [
+                                {
+                                    'id': 112112
+                                }
+                            ],
+                            'groupReferences': [
+                                {
+                                    'serviceGroupId': 51758,
+                                    'serviceId': 84986,
+                                    'weight': 2
+                                }
+                            ],
+                            'ipAddressId': 14288108,
+                            'port': 8080,
+                            'status': "DOWN"
+                        }
+                    ]
+                }
+            ],
+            "virtualIpAddress": {
+                'accountId': 307608,
+                'connectionLimit': 500,
+                'connectionLimitUnits': "connections/second",
+                'id': 22348,
+                'ipAddressId': 7303278,
+            },
+            'virtualIpAddressId': 22348
+        }]}
 getVirtualServers = [
     {
         "allocation": 10,
