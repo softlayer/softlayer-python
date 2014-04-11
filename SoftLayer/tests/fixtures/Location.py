465268e7c4fee66650030b7736beed11e554cabf
anasouma <waelriac@us.ibm.com>
2014-04-11 13:20:44 -0700

    Merge changes

---
+++ SoftLayer/tests/fixtures/Location.py
@@ -0,0 +1,11 @@
+getDataCenters = [
+    {
+        "id": 358694,
+        "longName": "London 2",
+        "name": "lon02"
+    },
+    {
+        "id": 168642,
+        "longName": "San Jose 1",
+        "name": "sjc01"
+    }]
