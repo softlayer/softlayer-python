26572ef03aa9f3e359a6a8e4dc36b1cb7b6978c7
Michael O'Rourke <michael.orourke@zoho.com>
2014-07-06 18:15:22 -0500

    add description to stub.js

--- plugins/stub.js
+++ plugins/stub.js
@@ -1,16 +1,21 @@
-
-// Hides console errors in browsers that lack a console
+/*
+#  Stub
+#  Hides console errors in browsers without a console
+#
+#  Copyright © Softlayer, an IBM Company
+#  Code and documentation licensed under MIT
+*/
 
 (function() {
     var method;
-    var noop = function() {};
+    var noop    = function() {};
     var methods = [
         "assert', "clear", "count", "debug", "dir", "dirxml", "error",
-        "exception', "group", "groupCollapsed", "groupEnd", "info", "log",
+        "exception", "group", "groupCollapsed", "groupEnd", "info", "log",
         "markTimeline", "profile", "profileEnd", "table", "time", "timeEnd",
         "timeStamp", "trace", "warn"
     ];
-    var length = methods.length;
+    var length  = methods.length;
     var console = (window.console = window.console || {});
 
     while (length--) {
