3309777a84af31314453abe33182eebcbc62e6ca
Michael O'Rourke <michael.orourke@zoho.com>
2014-07-06 18:12:14 -0500

    create stub.js

---
+++ plugins/stub.js
@@ -0,0 +1,23 @@
+
+// Hides console errors in browsers that lack a console
+
+(function() {
+    var method;
+    var noop = function() {};
+    var methods = [
+        "assert', "clear", "count", "debug", "dir", "dirxml", "error",
+        "exception', "group", "groupCollapsed", "groupEnd", "info", "log",
+        "markTimeline", "profile", "profileEnd", "table", "time", "timeEnd",
+        "timeStamp", "trace", "warn"
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
