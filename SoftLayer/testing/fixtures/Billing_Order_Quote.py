e0d075661677b4b02fa29d108472e80b9fbcad02
underscorephil <underscorephil@gmail.com>
2014-08-07 17:48:20 -0500

    Add quote fixture

---
+++ SoftLayer/testing/fixtures/Billing_Order_Quote.py
@@ -0,0 +1,18 @@
+getObject = {
+    'accountId': 1234,
+    'id': 1234,
+    'name': 'TestQuote1234',
+    'quoteKey': '1234test4321',
+}
+
+getRecalculatedOrderContainer = {
+    'orderContainers': [{
+        'presetId': '',
+        'prices': [{
+            'id': 1921
+        }],
+        'quantity': 1,
+        'packageId': 50,
+        'useHourlyPricing': '',
+    }],
+}
