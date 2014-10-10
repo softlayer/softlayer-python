7577307c1b2fca9d4d403b9de23ab6e34b674694
Kevin McDonald <k3vinmcdonald@gmail.com>
2014-10-10 12:47:19 -0500

    Version bump to 3.3.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ else:
 
 setup(
     name='SoftLayer',
-    version='3.2.0',
+    version='3.3.0',
     description=description,
     long_description=long_description,
     author='SoftLayer Technologies, Inc.',
