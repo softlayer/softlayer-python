ccc5dfdb67ca89ba6e2034d565c5e235962d8352
Kevin McDonald <k3vinmcdonald@gmail.com>
2014-11-13 10:47:38 -0600

    Renames sl command to slcli

---
+++ SoftLayer/CLI/deprecated.py
@@ -0,0 +1,16 @@
+"""
+    SoftLayer.CLI.deprecated
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+    Handles usage of the deprecated command name, 'sl'.
+
+    :license: MIT, see LICENSE for more details.
+"""
+import sys
+
+import click
+
+
+def main():
+    click.echo("ERORR: Use the 'slcli' command instead.")
+    click.echo("> slcli %s" % ' '.join(sys.argv[1:]))
+    exit(-1)
