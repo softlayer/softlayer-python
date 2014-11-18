d217faacd90cf982f5d5bf9463a5aa1f47789521
Kevin McDonald <k3vinmcdonald@gmail.com>
2014-11-18 17:35:57 -0600

    Update User-agent

--- SoftLayer/consts.py
+++ SoftLayer/consts.py
@@ -10,4 +10,4 @@ API_PUBLIC_ENDPOINT = 'https://api.softlayer.com/xmlrpc/v3.1/'
 API_PRIVATE_ENDPOINT = 'https://api.service.softlayer.com/xmlrpc/v3.1/'
 API_PUBLIC_ENDPOINT_REST = 'https://api.softlayer.com/rest/v3.1/'
 API_PRIVATE_ENDPOINT_REST = 'https://api.service.softlayer.com/rest/v3.1/'
-USER_AGENT = "SoftLayer Python %s" % VERSION
+USER_AGENT = "softlayer-python/%s" % VERSION
