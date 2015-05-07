fe5dc14f0002443a8f8a07e6b0a9cac601f5754c
Neetu Jain <nutshi@gmail.com>
2015-05-07 15:32:16 -0500

    pass the environment variable

--- SoftLayer/CLI/virt/ready.py
+++ SoftLayer/CLI/virt/ready.py
@@ -2,6 +2,7 @@
 # :license: MIT, see LICENSE for more details.
 
 import SoftLayer
+from SoftLayer.CLI import environment
 from SoftLayer.CLI import exceptions
 from SoftLayer.CLI import helpers
 
@@ -11,14 +12,13 @@ import click
 @click.command()
 @click.argument('identifier')
 @click.option('--wait', default=0, type=click.INT, help="Name of the image")
+@environment.pass_env
 def cli(env, identifier, wait):
     """Check if a virtual server is ready."""
 
     vsi = SoftLayer.VSManager(env.client)
-
     vs_id = helpers.resolve_id(vsi.resolve_ids, identifier, 'VS')
     ready = vsi.wait_for_ready(vs_id, wait)
-
     if ready:
         return "READY"
     else:
