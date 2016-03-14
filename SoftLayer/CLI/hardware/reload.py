54f645fad2867f45d40d94fc48813808c2e84ed7
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2016-03-14 13:53:21 -0500

    Corrected helpers text order
    Currently the Post install and SSH-key helper text are swapped 
    
    
    ```
    ❯ slcli --format raw server reload --help
    Usage: slcli server reload [OPTIONS] IDENTIFIER
    
      Reload operating system on a server.
    
    Options:
      -i, --postinstall TEXT  SSH keys to add to the root user
      -k, --key TEXT          Post-install script to download
                              (Only HTTPS
                              executes, HTTP leaves file in /root) (multiple
                              occurrence permitted)
      -h, --help              Show this message and exit.
    ```

--- SoftLayer/CLI/hardware/reload.py
+++ SoftLayer/CLI/hardware/reload.py
@@ -12,9 +12,9 @@ from SoftLayer.CLI import helpers
 
 @click.command()
 @click.argument('identifier')
-@click.option('--postinstall', '-i',
-              help=("Post-install script to download "
-                    "(Only HTTPS executes, HTTP leaves file in /root"))
+@helpers.multi_option('--postinstall', '-i',
+             help=("Post-install script to download "
+                   "(Only HTTPS executes, HTTP leaves file in /root"))
 @helpers.multi_option('--key', '-k', help="SSH keys to add to the root user")
 @environment.pass_env
 def cli(env, identifier, postinstall, key):
