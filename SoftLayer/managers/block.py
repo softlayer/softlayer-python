8e872436a6c331b1be584fb055982dc5f369c5cf
Tyler Britten <me@vmtyler.com>
2016-04-19 14:29:29 -0400

    expanded explanation

--- SoftLayer/managers/block.py
+++ SoftLayer/managers/block.py
@@ -149,7 +149,7 @@ class BlockStorageManager(utils.IdentifierMixin, object):
         try:
             location_id = self._get_location_id(location)
         except ValueError:
-            raise exceptions.SoftLayerError("Invalid datacenter name specified")
+            raise exceptions.SoftLayerError("Invalid datacenter name specified. Please provide the lower case short name (e.g.: dal09)")
 
         base_type_name = 'SoftLayer_Container_Product_Order_Network_'
         package = self._get_package(storage_type)
