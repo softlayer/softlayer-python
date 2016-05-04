b832bfa3c6a99c6bc2ee417e1cfcb0d2b941a528
Kevin McDonald <kmcdonald@softlayer.com>
2016-05-04 16:09:13 -0500

    Version bump to 5.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='SoftLayer',
-    version='5.0.1',
+    version='5.1.0',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SoftLayer Technologies, Inc.',
