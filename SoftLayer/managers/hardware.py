1d58edf99079dc8c688b4999340f9403b3a33703
Mark Whitney <markwhitney@gmail.com>
2016-09-01 23:25:30 -0700

    ssh keys for all, not just the first

--- SoftLayer/managers/hardware.py
+++ SoftLayer/managers/hardware.py
@@ -529,7 +529,9 @@ class HardwareManager(utils.IdentifierMixin, object):
             order['provisionScripts'] = [post_uri]
 
         if ssh_keys:
-            order['sshKeys'] = [{'sshKeyIds': ssh_keys}]
+            # need a copy of the key ids for each host, otherwise it
+            # will only set up keys on the first host
+            order['sshKeys'] = [{'sshKeyIds': ssh_keys}] * quantity
 
         return order
 
