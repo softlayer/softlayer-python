3e4be99128f15657706e73102d25123a592fd100
Christopher Gallo <chrisagallo@gmail.com>
2017-04-05 12:55:27 -0500

    Create setup.py
    bump to 5.2.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name='SoftLayer',
-    version='5.2.1',
+    version='5.2.3',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SoftLayer Technologies, Inc.',
