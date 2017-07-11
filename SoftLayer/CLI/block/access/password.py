e61c34b95ec2b2901caa6bbc7ddd63a632cc47e9
allmightyspiff <chrisagallo@gmail.com>
2017-07-11 17:13:17 -0500

    fixed documentation

--- SoftLayer/CLI/block/access/password.py
+++ SoftLayer/CLI/block/access/password.py
@@ -7,7 +7,7 @@ from SoftLayer.CLI import environment
 
 
 @click.command()
-@click.argument('access_id', "")
+@click.argument('access_id')
 @click.option('--password', '-p', multiple=False,
               help='Password you want to set, this command will fail if the password is not strong')
 @environment.pass_env
