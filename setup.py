b060f91b3f77ae74fbadfd890ee6e05f03f7a9f7
Christopher Gallo <chrisagallo@gmail.com>
2017-12-01 10:06:58 -0600

    version to 5.3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name='SoftLayer',
-    version='5.2.15',
+    version='5.3.0',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SoftLayer Technologies, Inc.',
