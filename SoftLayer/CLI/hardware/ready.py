59db25158f07742e693d2300e7e13bb68c47c411
allmightyspiff <chrisagallo@gmail.com>
2018-01-05 14:28:04 -0600

    actually adding the ready cli command

---
+++ SoftLayer/CLI/hardware/ready.py
@@ -0,0 +1,25 @@
+"""Check if a virtual server is ready."""
+# :license: MIT, see LICENSE for more details.
+
+import click
+
+import SoftLayer
+from SoftLayer.CLI import environment
+from SoftLayer.CLI import exceptions
+from SoftLayer.CLI import helpers
+
+
+@click.command()
+@click.argument('identifier')
+@click.option('--wait', default=0, show_default=True, type=click.INT, help="Seconds to wait")
+@environment.pass_env
+def cli(env, identifier, wait):
+    """Check if a virtual server is ready."""
+
+    compute = SoftLayer.HardwareManager(env.client)
+    compute_id = helpers.resolve_id(compute.resolve_ids, identifier, 'hardware')
+    ready = compute.wait_for_ready(compute_id, wait)
+    if ready:
+        env.fout("READY")
+    else:
+        raise exceptions.CLIAbort("Instance %s not ready" % compute_id)
