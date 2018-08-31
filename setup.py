71931ec8241b4d5cdf12149292c68d9a3bba4cb9
Christopher Gallo <chrisagallo@gmail.com>
2018-08-31 14:10:28 -0500

    5.5.2 release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name='SoftLayer',
-    version='5.5.1',
+    version='5.5.2',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SoftLayer Technologies, Inc.',
