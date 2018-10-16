ef4d507f1081e1cff693cf0fa7bac44c5fe542c6
Christopher Gallo <chrisagallo@gmail.com>
2018-10-16 17:20:48 -0500

    5.6.0 release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name='SoftLayer',
-    version='5.5.3',
+    version='5.6.0',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='SoftLayer Technologies, Inc.',
