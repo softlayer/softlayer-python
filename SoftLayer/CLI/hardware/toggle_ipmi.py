b2a09e6f5f246311471cec1d6da5153b7be3068f
ATGE <german.eduardo.qm@gmail.com>
2018-11-20 21:17:04 -0400

    #1059  Updating click.option to Boolean Flag

--- SoftLayer/CLI/hardware/toggle_ipmi.py
+++ SoftLayer/CLI/hardware/toggle_ipmi.py
@@ -10,14 +10,13 @@ from SoftLayer.CLI import helpers
 
 @click.command()
 @click.argument('identifier')
-@click.option('--enabled',
-              type=click.BOOL,
-              help="Whether to enable or disable the interface.")
+@click.option('--enable/--disable', default=True,
+              help="Whether enable (DEFAULT) or disable the interface.")
 @environment.pass_env
-def cli(env, identifier, enabled):
+def cli(env, identifier, enable):
     """Toggle the IPMI interface on and off"""
 
     mgr = SoftLayer.HardwareManager(env.client)
     hw_id = helpers.resolve_id(mgr.resolve_ids, identifier, 'hardware')
-    result = env.client['Hardware_Server'].toggleManagementInterface(enabled, id=hw_id)
+    result = env.client['Hardware_Server'].toggleManagementInterface(enable, id=hw_id)
     env.fout(result)
