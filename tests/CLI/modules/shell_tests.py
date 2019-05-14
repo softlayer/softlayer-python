105b9eef07968f46cd9d2f77e91d953b8e509a18
Cameron Porter <porterca@us.ibm.com>
2019-05-14 12:02:53 -0500

    Fix shell CLI tests.

--- tests/CLI/modules/shell_tests.py
+++ tests/CLI/modules/shell_tests.py
@@ -6,22 +6,24 @@
 """
 from SoftLayer import testing
 
-import mock
+import tempfile
 
 
 class ShellTests(testing.TestCase):
-    @mock.patch('prompt_toolkit.shortcuts.prompt')
-    def test_shell_quit(self, prompt):
-        prompt.return_value = "quit"
-        result = self.run_command(['shell'])
-        self.assertEqual(result.exit_code, 0)
 
-    @mock.patch('prompt_toolkit.shortcuts.prompt')
-    @mock.patch('shlex.split')
-    def test_shell_help(self, prompt, split):
-        split.side_effect = [(['help']), (['vs', 'list']), (False), (['quit'])]
-        prompt.return_value = "none"
-        result = self.run_command(['shell'])
-        if split.call_count is not 5:
-            raise Exception("Split not called correctly. Count: " + str(split.call_count))
-        self.assertEqual(result.exit_code, 1)
+    def test_shell_quit(self):
+        # Use a file as stdin
+        with tempfile.NamedTemporaryFile() as stdin:
+            stdin.write(b'exit\n')
+            stdin.seek(0)
+            result = self.run_command(['shell'], input=stdin)
+            self.assertEqual(result.exit_code, 0)
+
+    def test_shell_help(self):
+        # Use a file as stdin
+        with tempfile.NamedTemporaryFile() as stdin:
+            stdin.write(b'help\nexit\n')
+            stdin.seek(0)
+            result = self.run_command(['shell'], input=stdin)
+            self.assertIn('Welcome to the SoftLayer shell.', result.output)
+            self.assertEqual(result.exit_code, 0)
