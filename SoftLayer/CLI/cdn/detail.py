3ef580e9b4930bf7f04851fe4cc421663a3e0c82
Fernando Ojeda <fernando.ojeda@jalasoft.com>
2019-06-10 11:23:12 -0400

    Feature cdn network.

--- SoftLayer/CLI/cdn/detail.py
+++ SoftLayer/CLI/cdn/detail.py
@@ -9,24 +9,38 @@ from SoftLayer.CLI import formatting
 
 
 @click.command()
-@click.argument('account_id')
+@click.argument('unique_id')
+@click.option('--last_days',
+              default=30,
+              help='cdn overview last days less than 90 days, because it is the maximum e.g 7, 15, 30, 60, 89')
 @environment.pass_env
-def cli(env, account_id):
+def cli(env, unique_id, last_days):
     """Detail a CDN Account."""
 
     manager = SoftLayer.CDNManager(env.client)
-    account = manager.get_account(account_id)
+
+    cdn_mapping = manager.get_cdn(unique_id)
+    cdn_metrics = manager.get_usage_metrics(unique_id, days=last_days)
+
+    # usage metrics
+    total_bandwidth = str(cdn_metrics['totals'][0]) + " GB"
+    total_hits = str(cdn_metrics['totals'][1])
+    hit_radio = str(cdn_metrics['totals'][2]) + " %"
 
     table = formatting.KeyValueTable(['name', 'value'])
     table.align['name'] = 'r'
     table.align['value'] = 'l'
 
-    table.add_row(['id', account['id']])
-    table.add_row(['account_name', account['cdnAccountName']])
-    table.add_row(['type', account['cdnSolutionName']])
-    table.add_row(['status', account['status']['name']])
-    table.add_row(['created', account['createDate']])
-    table.add_row(['notes',
-                   account.get('cdnAccountNote', formatting.blank())])
+    table.add_row(['unique_id', cdn_mapping['uniqueId']])
+    table.add_row(['hostname', cdn_mapping['domain']])
+    table.add_row(['protocol', cdn_mapping['protocol']])
+    table.add_row(['origin', cdn_mapping['originHost']])
+    table.add_row(['origin_type', cdn_mapping['originType']])
+    table.add_row(['path', cdn_mapping['path']])
+    table.add_row(['provider', cdn_mapping['vendorName']])
+    table.add_row(['status', cdn_mapping['status']])
+    table.add_row(['total_bandwidth', total_bandwidth])
+    table.add_row(['total_hits', total_hits])
+    table.add_row(['hit_radio', hit_radio])
 
     env.fout(table)
---
+++ SoftLayer/fixtures/SoftLayer_Network_CdnMarketplace_Configuration_Mapping.py
@@ -0,0 +1,31 @@
+listDomainMappings = [
+    {
+        "cname": "cdnakauuiet7s6u6.cdnedge.bluemix.net",
+        "domain": "test.example.com",
+        "header": "test.example.com",
+        "httpPort": 80,
+        "originHost": "1.1.1.1",
+        "originType": "HOST_SERVER",
+        "path": "/",
+        "protocol": "HTTP",
+        "status": "CNAME_CONFIGURATION",
+        "uniqueId": "9934111111111",
+        "vendorName": "akamai"
+    }
+]
+
+listDomainMappingByUniqueId = [
+    {
+        "cname": "cdnakauuiet7s6u6.cdnedge.bluemix.net",
+        "domain": "test.example.com",
+        "header": "test.example.com",
+        "httpPort": 80,
+        "originHost": "1.1.1.1",
+        "originType": "HOST_SERVER",
+        "path": "/",
+        "protocol": "HTTP",
+        "status": "CNAME_CONFIGURATION",
+        "uniqueId": "9934111111111",
+        "vendorName": "akamai"
+    }
+]
