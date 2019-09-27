53c17e1766d07d78bca11115aaad1bdb3e77618f
allmightyspiff <chrisagallo@gmail.com>
2019-09-27 16:59:39 -0500

    #627 autoscale feature documentation

--- SoftLayer/managers/autoscale.py
+++ SoftLayer/managers/autoscale.py
@@ -1,6 +1,6 @@
 """
     SoftLayer.autoscale
-    ~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~
     Autoscale manager
 
     :license: MIT, see LICENSE for more details.
@@ -14,10 +14,12 @@ class AutoScaleManager(object):
         self.client = client
 
     def list(self, mask=None):
-        """Calls SoftLayer_Account getScaleGroups()_
+        """Calls `SoftLayer_Account::getScaleGroups()`_
 
         :param mask: optional SoftLayer_Scale_Group objectMask
-        .. getScaleGroups(): https://sldn.softlayer.com/reference/services/SoftLayer_Account/getScaleGroups/
+
+        .. _SoftLayer_Account::getScaleGroups(): 
+            https://sldn.softlayer.com/reference/services/SoftLayer_Account/getScaleGroups/
         """
         if not mask:
             mask = "mask[status,virtualGuestMemberCount]"
@@ -25,11 +27,13 @@ class AutoScaleManager(object):
         return self.client.call('SoftLayer_Account', 'getScaleGroups', mask=mask, iter=True)
 
     def details(self, identifier, mask=None):
-        """Calls SoftLayer_Scale_Group getObject()_
+        """Calls `SoftLayer_Scale_Group::getObject()`_
 
         :param identifier: SoftLayer_Scale_Group id
         :param mask: optional SoftLayer_Scale_Group objectMask
-        .. _getObject(): https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Group/getObject/
+
+        .. _SoftLayer_Scale_Group::getObject(): 
+            https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Group/getObject/
         """
         if not mask:
             mask = """mask[virtualGuestMembers[id,virtualGuest[hostname,domain,provisionDate]], terminationPolicy,
@@ -40,11 +44,13 @@ class AutoScaleManager(object):
         return self.client.call('SoftLayer_Scale_Group', 'getObject', id=identifier, mask=mask)
 
     def get_policy(self, identifier, mask=None):
-        """Calls SoftLayer_Scale_Policy getObject()_
+        """Calls `SoftLayer_Scale_Policy::getObject()`_
 
         :param identifier: SoftLayer_Scale_Policy id
         :param mask: optional SoftLayer_Scale_Policy objectMask
-        .. _getObject(): https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Policy/getObject/
+
+        .. _SoftLayer_Scale_Policy::getObject(): 
+            https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Policy/getObject/
         """
         if not mask:
             mask = """mask[cooldown, createDate, id, name, actions, triggers[type]
@@ -54,31 +60,36 @@ class AutoScaleManager(object):
         return self.client.call('SoftLayer_Scale_Policy', 'getObject', id=identifier, mask=mask)
 
     def scale(self, identifier, amount):
-        """Calls SoftLayer_Scale_Group scale()_
+        """Calls `SoftLayer_Scale_Group::scale()`_
 
         :param identifier: SoftLayer_Scale_Group Id
         :param amount: positive or negative number to scale the group by
 
-        .. _scale(): https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Group/scale/
+        .. _SoftLayer_Scale_Group::scale(): 
+            https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Group/scale/
         """
         return self.client.call('SoftLayer_Scale_Group', 'scale', amount, id=identifier)
 
     def scale_to(self, identifier, amount):
-        """Calls SoftLayer_Scale_Group scaleTo()_
+        """Calls `SoftLayer_Scale_Group::scaleTo()`_
 
         :param identifier: SoftLayer_Scale_Group Id
         :param amount: number to scale the group to.
-        .. _scaleTo(): https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Group/scaleTo/
+
+        .. _SoftLayer_Scale_Group::scaleTo(): 
+            https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Group/scaleTo/
         """
         return self.client.call('SoftLayer_Scale_Group', 'scaleTo', amount, id=identifier)
 
     def get_logs(self, identifier, mask=None, object_filter=None):
-        """Calls SoftLayer_Scale_Group getLogs()_
+        """Calls `SoftLayer_Scale_Group::getLogs()`_
 
         :param identifier: SoftLayer_Scale_Group Id
         :param mask: optional SoftLayer_Scale_Group_Log objectMask
         :param object_filter: optional SoftLayer_Scale_Group_Log objectFilter
-        .. getLogs(): https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Group/getLogs/
+
+        .. _SoftLayer_Scale_Group::getLogs(): 
+            https://sldn.softlayer.com/reference/services/SoftLayer_Scale_Group/getLogs/
         """
         return self.client.call('SoftLayer_Scale_Group', 'getLogs', id=identifier, mask=mask, filter=object_filter,
                                 iter=True)
---
+++ docs/cli/autoscale.rst
@@ -0,0 +1,31 @@
+.. _cli_autoscale:
+
+Autoscale Commands
+==================
+These commands were added in version `5.8.1 <https://github.com/softlayer/softlayer-python/releases/tag/v5.8.1>`_
+
+For making changes to the triggers or the autoscale group itself, see the `Autoscale Portal`_
+
+- `Autoscale Product <https://www.ibm.com/cloud/auto-scaling>`_
+- `Autoscale Documentation <https://cloud.ibm.com/docs/vsi?topic=virtual-servers-about-auto-scale>`_
+- `Autoscale Portal`_
+
+.. click:: SoftLayer.CLI.autoscale.list:cli
+    :prog: autoscale list
+    :show-nested:
+
+.. click:: SoftLayer.CLI.autoscale.detail:cli
+    :prog: autoscale detail
+    :show-nested:
+
+.. click:: SoftLayer.CLI.autoscale.scale:cli
+    :prog: autoscale scale
+    :show-nested:
+
+.. click:: SoftLayer.CLI.autoscale.logs:cli
+    :prog: autoscale logs
+    :show-nested:
+
+
+
+.. _Autoscale Portal: https://cloud.ibm.com/classic/autoscale
