292d4fac6d77d54904ae6c5d79fca0894c1f1f27
Daniel Cabero Barrios <Daniel Cabero@jalasoft.local>
2019-10-03 15:59:30 -0400

    unit test logs and scale

--- SoftLayer/fixtures/SoftLayer_Scale_Group.py
+++ SoftLayer/fixtures/SoftLayer_Scale_Group.py
@@ -1,12 +1,53 @@
 getLogs = [
     {
-        "createDate": "2018-04-23T14:22:52-06:00",
-        "description": "Scaling group to 3 member(s) by adding 3 member(s) as manually requested",
-        "id": 123456,
+        "createDate": "2019-10-02T08:34:21-06:00",
+        "id": 3145330,
+        "scaleGroupId": 224541
     }, {
-        "createDate": "2018-04-23T14:22:52-06:00",
-        "description": "Scaling group to 3 member(s) by adding 3 member(s) as manually requested",
-        "id": 987123456,
+        "createDate": "2019-10-02T08:34:21-06:00",
+        "id": 3145330,
+        "scaleGroupId": 2245415
+
     }
 ]
 
+scale=[]
+
+scaleTo=[{'createDate': '2019-10-02T15:24:56-06:00',
+            'id': 3145162,
+            'scaleGroupId': 595465,
+            'scaleGroup': {},
+            'virtualGuest': {
+                'accountId': 12369852,
+                             'createDate': '2019 - 10 - 02T15: 24:54 - 06: 00',
+                             'billingItem': {
+                                 "cancellationDate": '2019-10-02T08:34:21-06:00'
+                             },
+                             'domain': 'mesos.maceacs.com',
+                             'fullyQualifiedDomainName': '2 - master - 600e.mesos.maceacs.com',
+                             'hostname': '2 - master - 600e',
+                             'id': 852369,
+                             'maxCpu': 4,
+                             'maxCpuUnits': 'CORE',
+                             'maxMemory': 8192,
+                             'startCpus': 4,
+                             'statusId': 1001,
+                             'typeId': 1,
+                             'uuid': 'acva18321 - bb6b - 4861 - 87f3 - 8a6dbbcf148b',
+                             'activeTransactions': [{
+                                 'createDate': '2019 - 10 - 02T15: 24:58 - 06: 00',
+                                 'elapsedSeconds': 2,
+                                 'guestId': 789654123,
+                                 'hardwareId': '',
+                                 'id': 456987,
+                                 'modifyDate': '2019 - 10 - 02T15: 24:58 - 06: 00',
+                                 'statusChangeDate': '2019 - 10 - 02T15: 24:58 - 06: 00',
+                                 'transactionStatus': {
+                                     'averageDuration': '1.48',
+                                     'friendlyName': 'AssignHost',
+                                     'name': 'ASSIGN_HOST'}}],
+                             'status': {
+                                 'keyName': 'ACTIVE',
+                                 'name': 'Active'}
+                             }
+        }]
---
+++ tests/CLI/modules/autoscale_tests.py
@@ -0,0 +1,35 @@
+"""
+    SoftLayer.tests.CLI.modules.autoscale_tests
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :license: MIT, see LICENSE for more details.
+"""
+from SoftLayer import testing
+
+
+class AutoscaleTests(testing.TestCase):
+
+    def test_logs_dates(self):
+        result = self.run_command(['autoscale', 'logs', '123456', '-d', '2019-02-02'])
+        print(result)
+        self.assert_no_fail(result)
+
+    def test_scale_down(self):
+        result = self.run_command(['autoscale', 'scale', '123456', '--down', '--amount', '2'])
+        self.assert_no_fail(result)
+
+    def test_scale_up(self):
+        result = self.run_command(['autoscale', 'scale', '123456', '--up', '--amount', '2'])
+        self.assert_no_fail(result)
+
+    def test_scale_to(self):
+        result = self.run_command(['autoscale', 'scale', '789654123', '--down', '--amount', '2'])
+        self.assert_no_fail(result)
+
+    def test_scale_by_up(self):
+        result = self.run_command(['autoscale', 'scale', '789654123', '--by', '--down', '--amount', '-1'])
+        self.assert_no_fail(result)
+
+    def test_scale_ca(self):
+        result = self.run_command(['autoscale', 'scale', '789654123', '--by', '--down', '--amount', '1'])
+        self.assert_no_fail(result)
