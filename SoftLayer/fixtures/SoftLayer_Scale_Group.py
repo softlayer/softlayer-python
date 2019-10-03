5cc773755a89105940fb352b7f70a7422e689b0e
Daniel Cabero Barrios <Daniel Cabero@jalasoft.local>
2019-10-03 16:08:49 -0400

    fix the tox analysis

--- SoftLayer/fixtures/SoftLayer_Scale_Group.py
+++ SoftLayer/fixtures/SoftLayer_Scale_Group.py
@@ -11,43 +11,44 @@ getLogs = [
     }
 ]
 
-scale=[]
+scale = []
 
-scaleTo=[{'createDate': '2019-10-02T15:24:56-06:00',
+scaleTo = [{'createDate': '2019-10-02T15:24:56-06:00',
             'id': 3145162,
             'scaleGroupId': 595465,
             'scaleGroup': {},
             'virtualGuest': {
                 'accountId': 12369852,
-                             'createDate': '2019 - 10 - 02T15: 24:54 - 06: 00',
-                             'billingItem': {
-                                 "cancellationDate": '2019-10-02T08:34:21-06:00'
-                             },
-                             'domain': 'mesos.maceacs.com',
-                             'fullyQualifiedDomainName': '2 - master - 600e.mesos.maceacs.com',
-                             'hostname': '2 - master - 600e',
-                             'id': 852369,
-                             'maxCpu': 4,
-                             'maxCpuUnits': 'CORE',
-                             'maxMemory': 8192,
-                             'startCpus': 4,
-                             'statusId': 1001,
-                             'typeId': 1,
-                             'uuid': 'acva18321 - bb6b - 4861 - 87f3 - 8a6dbbcf148b',
-                             'activeTransactions': [{
-                                 'createDate': '2019 - 10 - 02T15: 24:58 - 06: 00',
-                                 'elapsedSeconds': 2,
-                                 'guestId': 789654123,
-                                 'hardwareId': '',
-                                 'id': 456987,
-                                 'modifyDate': '2019 - 10 - 02T15: 24:58 - 06: 00',
-                                 'statusChangeDate': '2019 - 10 - 02T15: 24:58 - 06: 00',
-                                 'transactionStatus': {
-                                     'averageDuration': '1.48',
-                                     'friendlyName': 'AssignHost',
-                                     'name': 'ASSIGN_HOST'}}],
-                             'status': {
-                                 'keyName': 'ACTIVE',
-                                 'name': 'Active'}
-                             }
-        }]
+                'createDate': '2019 - 10 - 02T15: 24:54 - 06: 00',
+                'billingItem': {
+                    "cancellationDate": '2019-10-02T08:34:21-06:00'
+                },
+                'domain': 'mesos.maceacs.com',
+                'fullyQualifiedDomainName': '2 - master - 600e.mesos.maceacs.com',
+                'hostname': '2 - master - 600e',
+                'id': 852369,
+                'maxCpu': 4,
+                'maxCpuUnits': 'CORE',
+                'maxMemory': 8192,
+                'startCpus': 4,
+                'statusId': 1001,
+                'typeId': 1,
+                'uuid': 'acva18321 - bb6b - 4861 - 87f3 - 8a6dbbcf148b',
+                'activeTransactions': [{
+                    'createDate': '2019 - 10 - 02T15: 24:58 - 06: 00',
+                    'elapsedSeconds': 2,
+                    'guestId': 789654123,
+                    'hardwareId': '',
+                    'id': 456987,
+                    'modifyDate': '2019 - 10 - 02T15: 24:58 - 06: 00',
+                    'statusChangeDate': '2019 - 10 - 02T15: 24:58 - 06: 00',
+                    'transactionStatus': {
+                        'averageDuration': '1.48',
+                        'friendlyName': 'AssignHost',
+                        'name': 'ASSIGN_HOST'}}],
+                'status': {
+                    'keyName': 'ACTIVE',
+                    'name': 'Active'}
+            }
+            }
+           ]
