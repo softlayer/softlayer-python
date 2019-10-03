d027f82fdc68b4b709c75c82882a3b5c9d5c9d40
Daniel Cabero Barrios <Daniel Cabero@jalasoft.local>
2019-10-03 15:54:11 -0400

    unit test logs and scale

---
+++ SoftLayer/fixtures/SoftLayer_Scale_Group.py
@@ -0,0 +1,12 @@
+getLogs = [
+    {
+        "createDate": "2018-04-23T14:22:52-06:00",
+        "description": "Scaling group to 3 member(s) by adding 3 member(s) as manually requested",
+        "id": 123456,
+    }, {
+        "createDate": "2018-04-23T14:22:52-06:00",
+        "description": "Scaling group to 3 member(s) by adding 3 member(s) as manually requested",
+        "id": 987123456,
+    }
+]
+
