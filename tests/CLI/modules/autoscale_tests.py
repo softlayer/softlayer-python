29ab911aa399085aad81da8a7ea95997a2afb359
ATGE <german.eduardo.qm@gmail.com>
2019-10-04 12:23:55 -0400

    added autoscale detail test

--- tests/CLI/modules/autoscale_tests.py
+++ tests/CLI/modules/autoscale_tests.py
@@ -2,7 +2,7 @@
     SoftLayer.tests.CLI.modules.autoscale_tests
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    Tests for the user cli command
+    Tests for the autoscale cli command
 """
 from SoftLayer import testing
 
@@ -11,4 +11,8 @@ class AutoScaleTests(testing.TestCase):
 
     def test_autoscale_list(self):
         result = self.run_command(['autoscale', 'list'])
+        self.assert_no_fail(result)
+
+    def test_autoscale_detail(self):
+        result = self.run_command(['autoscale', 'detail', '12222222'])
         self.assert_no_fail(result)
