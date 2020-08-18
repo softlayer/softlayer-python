00cc11f4dc2baedd632f039288b501071628e97a
caberos <cabermaldo010788@gmail.com>
2020-08-18 09:56:57 -0400

    fix the ha option firewall add and implement unit test

--- tests/CLI/modules/firewall_tests.py
+++ tests/CLI/modules/firewall_tests.py
@@ -5,6 +5,9 @@
     :license: MIT, see LICENSE for more details.
 """
 import json
+from unittest import mock
+
+from SoftLayer.CLI import exceptions
 
 from SoftLayer import testing
 
@@ -28,3 +31,29 @@ class FirewallTests(testing.TestCase):
                            'firewall id': 'server:1234',
                            'server/vlan id': 1,
                            'type': 'Server - standard'}])
+
+    @mock.patch('SoftLayer.CLI.formatting.confirm')
+    def test_add_vs(self, confirm_mock):
+        confirm_mock.return_value = True
+        result = self.run_command(['firewall', 'add', '1000', '--firewall-type=vlan', '-ha'])
+        self.assert_no_fail(result)
+        self.assertIn("Firewall is being created!", result.output)
+
+    @mock.patch('SoftLayer.CLI.formatting.confirm')
+    def test_add_vlan(self, confirm_mock):
+        confirm_mock.return_value = True
+        result = self.run_command(['firewall', 'add', '1000', '--firewall-type=vs'])
+        self.assert_no_fail(result)
+        self.assertIn("Firewall is being created!", result.output)
+
+    @mock.patch('SoftLayer.CLI.formatting.confirm')
+    def test_add_server(self, confirm_mock):
+        confirm_mock.return_value = True
+        result = self.run_command(['firewall', 'add', '1000', '--firewall-type=server'])
+        self.assert_no_fail(result)
+        self.assertIn("Firewall is being created!", result.output)
+
+    def test_add_server_fail(self):
+        result = self.run_command(['firewall', 'add', '1000', '--firewall-type=server'])
+        self.assertEqual(result.exit_code, 2)
+        self.assertIsInstance(result.exception, exceptions.CLIAbort)
