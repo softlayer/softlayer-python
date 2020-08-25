4450dbcc77b8930fa40c8691bff6eaf9aba463a0
caberos <cabermaldo010788@gmail.com>
2020-08-25 15:19:00 -0400

    fix and update the tox tool

--- SoftLayer/fixtures/SoftLayer_Security_Certificate.py
+++ SoftLayer/fixtures/SoftLayer_Security_Certificate.py
@@ -2,7 +2,7 @@ getObject = {
     "certificate": "-----BEGIN CERTIFICATE----- \nMIIEJTCCAw2gAwIBAgIDCbQ0MA0GCSqGSIb3DQEBCwUAMEcxCzAJBgNVBAYTAlVT"
                    " -----END CERTIFICATE-----",
     "certificateSigningRequest": "-----BEGIN CERTIFICATE REQUEST-----\n"
-                                 "MIIC1jCCAb4CAQAwgZAxCzAJBgNVBAYTAlVTMQ4wDAYDVQQIDAVUZXhhczEQMA4G\n"
+                                 "MIIC1jCCAb4CAQAwgZAxCzAJBgNVBAYTAlVTMQ4wDAYDVQQIDAVUZXhh123456QMA4G\n"
                                  "-----END CERTIFICATE REQUEST-----",
     "commonName": "techbabble.xyz",
     "createDate": "2016-01-20T10:56:44-06:00",
@@ -11,7 +11,7 @@ getObject = {
     "keySize": 258369,
     "modifyDate": "2019-06-05T14:10:40-06:00",
     "organizationName": "Unspecified",
-    "privateKey": "-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEA3SwTZ7sh7we5zIbmtSbxGJxf67Ayf07eutrK8OAjWXtwQSdE\n"
+    "privateKey": "-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEA3SwTZ7sh7we5zIbmtSbxGJxff07eutrK12345678WXtwQSdE\n"
                   "-----END RSA PRIVATE KEY-----",
     "validityBegin": "2016-01-19T17:59:37-06:00",
     "validityDays": 0,
