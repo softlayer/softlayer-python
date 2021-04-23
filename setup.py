1039f158b13fd5a9f5725e68285e5e5b651188cf
Christopher Gallo <chrisagallo@gmail.com>
2021-04-23 15:28:35 -0500

    dropping support for py3.5 as it is EOL https://www.python.org/downloads/release/python-3510/

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
