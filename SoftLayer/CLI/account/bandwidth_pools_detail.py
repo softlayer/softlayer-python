eee36d53381d34c427c81f050b06c5e0664c70e5
caberos <Daniel.Cabero@ibm.com>
2022-02-11 08:59:37 -0400

    fix the some problems and fix the team code review comments

--- SoftLayer/CLI/account/bandwidth_pools_detail.py
+++ SoftLayer/CLI/account/bandwidth_pools_detail.py
@@ -12,7 +12,7 @@ from SoftLayer import utils
 @click.argument('identifier')
 @environment.pass_env
 def cli(env, identifier):
-    """Get bandwidth about a VLAN."""
+    """Get bandwidth pool details."""
 
     manager = AccountManager(env.client)
     bandwidths = manager.getBandwidthDetail(identifier)
@@ -23,9 +23,12 @@ def cli(env, identifier):
     table.add_row(['Id', bandwidths['id']])
     table.add_row(['Name', bandwidths['name']])
     table.add_row(['Create Date', bandwidths['createDate']])
-    table.add_row(['Current Usage', bandwidths['billingCyclePublicBandwidthUsage']['amountOut']])
-    table.add_row(['Projected  Usage', bandwidths['projectedPublicBandwidthUsage']])
-    table.add_row(['Inbound   Usage', bandwidths['inboundPublicBandwidthUsage']])
+    current = "{} GB".format(bandwidths['billingCyclePublicBandwidthUsage']['amountOut'], 0)
+    table.add_row(['Current Usage', current])
+    projected = "{} GB".format(bandwidths.get('projectedPublicBandwidthUsage', 0))
+    table.add_row(['Projected  Usage', projected])
+    inbound = "{} GB".format(bandwidths.get('inboundPublicBandwidthUsage', 0))
+    table.add_row(['Inbound   Usage', inbound])
     if bandwidths['hardware'] != []:
         table.add_row(['hardware', _bw_table(bandwidths['hardware'])])
     else:
@@ -48,8 +51,8 @@ def _bw_table(bw_data):
     """Generates a bandwidth useage table"""
     table_data = formatting.Table(['Id', 'HostName', "IP Address", 'Amount', "Current Usage"])
     for bw_point in bw_data:
-        amount = bw_point['bandwidthAllotmentDetail']['allocation']['amount']
-        current = utils.lookup(bw_point, 'outboundBandwidthUsage')
+        amount = "{} GB".format(utils.lookup(bw_point, 'bandwidthAllotmentDetail', 'allocation', 'amountOut'), 0)
+        current = "{} GB".format(utils.lookup(bw_point, 'outboundBandwidthUsage'), 0)
         ip_address = bw_point['primaryIpAddress']
         table_data.add_row([bw_point['id'], bw_point['fullyQualifiedDomainName'], ip_address, amount, current])
     return [table_data]
@@ -59,8 +62,8 @@ def _virtual_table(bw_data):
     """Generates a virtual bandwidth usage table"""
     table_data = formatting.Table(['Id', 'HostName', "IP Address", 'Amount', "Current Usage"])
     for bw_point in bw_data:
-        amount = bw_point['bandwidthAllotmentDetail']['allocation']['amount']
-        current = utils.lookup(bw_point, 'outboundPublicBandwidthUsage')
+        amount = "{} GB".format(utils.lookup(bw_point, 'bandwidthAllotmentDetail', 'allocation', 'amountOut'), 0)
+        current = "{} GB".format(utils.lookup(bw_point, 'outboundBandwidthUsage'), 0)
         ip_address = bw_point['primaryIpAddress']
         table_data.add_row([bw_point['id'], bw_point['fullyQualifiedDomainName'], ip_address, amount, current])
     return [table_data]
