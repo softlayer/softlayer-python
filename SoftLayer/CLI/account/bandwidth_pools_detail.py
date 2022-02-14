6f8590690b22d3e076a6f9b2baf4cf42656bf326
caberos <Daniel.Cabero@ibm.com>
2022-02-14 09:59:13 -0400

    fix tox tool and fix the some problems

--- SoftLayer/CLI/account/bandwidth_pools_detail.py
+++ SoftLayer/CLI/account/bandwidth_pools_detail.py
@@ -22,17 +22,23 @@ def cli(env, identifier):
     table.align['value'] = 'l'
     table.add_row(['Id', bandwidths['id']])
     table.add_row(['Name', bandwidths['name']])
-    table.add_row(['Create Date', bandwidths['createDate']])
+    table.add_row(['Create Date', utils.clean_time(bandwidths.get('createDate'), '%Y-%m-%d')])
     current = "{} GB".format(utils.lookup(bandwidths, 'billingCyclePublicBandwidthUsage', 'amountOut'))
+    if current is None:
+        current = '-'
     table.add_row(['Current Usage', current])
     projected = "{} GB".format(bandwidths.get('projectedPublicBandwidthUsage', 0))
+    if projected is None:
+        projected = '-'
     table.add_row(['Projected  Usage', projected])
     inbound = "{} GB".format(bandwidths.get('inboundPublicBandwidthUsage', 0))
+    if inbound is None:
+        inbound = '-'
     table.add_row(['Inbound   Usage', inbound])
     if bandwidths['hardware'] != []:
         table.add_row(['hardware', _bw_table(bandwidths['hardware'])])
     else:
-        table.add_row(['hardware', 'not found'])
+        table.add_row(['hardware', 'Not Found'])
 
     if bandwidths['virtualGuests'] != []:
         table.add_row(['virtualGuests', _virtual_table(bandwidths['virtualGuests'])])
@@ -40,9 +46,9 @@ def cli(env, identifier):
         table.add_row(['virtualGuests', 'Not Found'])
 
     if bandwidths['bareMetalInstances'] != []:
-        table.add_row(['Netscale', _bw_table(bandwidths['bareMetalInstances'])])
+        table.add_row(['Netscaler', _bw_table(bandwidths['bareMetalInstances'])])
     else:
-        table.add_row(['Netscale', 'Not Found'])
+        table.add_row(['Netscaler', 'Not Found'])
 
     env.fout(table)
 
@@ -51,9 +57,11 @@ def _bw_table(bw_data):
     """Generates a bandwidth useage table"""
     table_data = formatting.Table(['Id', 'HostName', "IP Address", 'Amount', "Current Usage"])
     for bw_point in bw_data:
-        amount = "{} GB".format(utils.lookup(bw_point, 'bandwidthAllotmentDetail', 'allocation', 'amountOut'))
+        amount = "{} GB".format(utils.lookup(bw_point, 'bandwidthAllotmentDetail', 'allocation', 'amount'))
         current = "{} GB".format(bw_point.get('outboundBandwidthUsage', 0))
-        ip_address = bw_point['primaryIpAddress']
+        ip_address = bw_point.get('primaryIpAddress')
+        if ip_address is None:
+            ip_address = '-'
         table_data.add_row([bw_point['id'], bw_point['fullyQualifiedDomainName'], ip_address, amount, current])
     return [table_data]
 
@@ -62,8 +70,10 @@ def _virtual_table(bw_data):
     """Generates a virtual bandwidth usage table"""
     table_data = formatting.Table(['Id', 'HostName', "IP Address", 'Amount', "Current Usage"])
     for bw_point in bw_data:
-        amount = "{} GB".format(utils.lookup(bw_point, 'bandwidthAllotmentDetail', 'allocation', 'amountOut'))
+        amount = "{} GB".format(utils.lookup(bw_point, 'bandwidthAllotmentDetail', 'allocation', 'amount'))
         current = "{} GB".format(bw_point.get('outboundBandwidthUsage', 0))
-        ip_address = bw_point['primaryIpAddress']
+        ip_address = bw_point.get('primaryIpAddress')
+        if ip_address is None:
+            ip_address = '-'
         table_data.add_row([bw_point['id'], bw_point['fullyQualifiedDomainName'], ip_address, amount, current])
     return [table_data]
