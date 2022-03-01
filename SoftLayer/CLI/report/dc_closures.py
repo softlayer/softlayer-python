da2273ee50e983868065270f0a3445f108b59526
Christopher Gallo <chrisagallo@gmail.com>
2022-03-01 17:49:45 -0600

    #1590 added docs and unit tests

--- SoftLayer/CLI/report/dc_closures.py
+++ SoftLayer/CLI/report/dc_closures.py
@@ -1,8 +1,4 @@
-"""Metric Utilities"""
-import datetime
-import itertools
-import sys
-
+"""Report on Resources in closing datacenters"""
 import click
 
 from SoftLayer.CLI import environment
@@ -10,14 +6,12 @@ from SoftLayer.CLI import formatting
 from SoftLayer import utils
 
 
-from pprint import pprint as pp
-
 @click.command(short_help="""Report on Resources in closing datacenters""")
 @environment.pass_env
 def cli(env):
     """Report on Resources in closing datacenters
 
-    Displays a list of Datacenters soon to be shutdown, and any resources on the account 
+    Displays a list of Datacenters soon to be shutdown, and any resources on the account
 in those locations
     """
 
@@ -33,7 +27,7 @@ in those locations
     }
     mask = """mask[name, datacenterLongName, frontendRouterId, capabilities, datacenterId, backendRouterId,
 backendRouterName, frontendRouterName]"""
-    closing_pods = env.client.call('SoftLayer_Network_Pod', 'getAllObjects', mask=mask)
+    closing_pods = env.client.call('SoftLayer_Network_Pod', 'getAllObjects', mask=mask, filter=closing_filter)
     # Find all VLANs in the POD that is going to close.
     search = "_objectType:SoftLayer_Network_Vlan  primaryRouter.hostname: \"{}\" || primaryRouter.hostname: \"{}\""
     resource_mask = """mask[
@@ -54,16 +48,17 @@ backendRouterName, frontendRouterName]"""
     for pod in closing_pods:
         resources = {'hardware': {}, 'virtual': {}, 'firewall': {}, 'gateway': {}}
         vlans = env.client.call('SoftLayer_Search', 'advancedSearch',
-                                    search.format(pod.get('backendRouterName'), pod.get('frontendRouterName')),
-                                    iter=True, mask=resource_mask)
+                                search.format(pod.get('backendRouterName'), pod.get('frontendRouterName')),
+                                iter=True, mask=resource_mask)
+        # Go through the vlans and coalate the resources into a data structure that is easy to print out
         for vlan in vlans:
             resources = process_vlan(vlan.get('resource', {}), resources)
-        
-        for resource_type in resources.keys():
-            
-            for resource_object in resources[resource_type].values():
+
+        # Go through each resource and add it to the table
+        for resource_type, resource_values in resources.items():
+            for resource_id, resource_object in resource_values.items():
                 resource_table.add_row([
-                    resource_object['id'],
+                    resource_id,
                     resource_object['name'],
                     resource_object['vlan'].get('PUBLIC', '-'),
                     resource_object['vlan'].get('PRIVATE', '-'),
@@ -72,46 +67,51 @@ backendRouterName, frontendRouterName]"""
                     pod.get('backendRouterName'),
                     resource_object['cancelDate']
                 ])
-        
+
     env.fout(resource_table)
 
 
 # returns a Table Row for a given resource
 def process_vlan(vlan, resources=None):
+    """Takes in a vlan object and pulls out the needed resources"""
     if resources is None:
         resources = {'hardware': {}, 'virtual': {}, 'firewall': {}, 'gateway': {}}
 
     type_x = "virtual"
-    for x in vlan.get('virtualGuests', {}):
-        existing = resources[type_x].get(x.get('id'))
-        resources[type_x][x['id']] = build_resource_object('fullyQualifiedDomainName', vlan, x, existing)
+    for obj_x in vlan.get('virtualGuests', {}):
+        existing = resources[type_x].get(obj_x.get('id'))
+        resources[type_x][obj_x['id']] = build_resource_object('fullyQualifiedDomainName', vlan, obj_x, existing)
 
     type_x = 'hardware'
-    for x in vlan.get('hardware', {}):
-        existing = resources[type_x].get(x.get('id'))
-        resources[type_x][x['id']] = build_resource_object('fullyQualifiedDomainName', vlan, x, existing)
+    for obj_x in vlan.get('hardware', {}):
+        existing = resources[type_x].get(obj_x.get('id'))
+        resources[type_x][obj_x['id']] = build_resource_object('fullyQualifiedDomainName', vlan, obj_x, existing)
 
     type_x = 'firewall'
-    for x in vlan.get('networkVlanFirewall', {}):
-        existing = resources[type_x].get(x.get('id'))
-        resources[type_x][x['id']] = build_resource_object('primaryIpAddress', vlan, x, existing)
+    for obj_x in vlan.get('networkVlanFirewall', {}):
+        existing = resources[type_x].get(obj_x.get('id'))
+        resources[type_x][obj_x['id']] = build_resource_object('primaryIpAddress', vlan, obj_x, existing)
 
     type_x = 'gateway'
-    for x in vlan.get('privateNetworkGateways', {}):
-        existing = resources[type_x].get(x.get('id'))
-        resources[type_x][x['id']] = build_resource_object('name', vlan, x, existing)
-    for x in vlan.get('publicNetworkGateways', {}):
-        existing = resources[type_x].get(x.get('id'))
-        resources[type_x][x['id']] = build_resource_object('name', vlan, x, existing)
+    for obj_x in vlan.get('privateNetworkGateways', {}):
+        existing = resources[type_x].get(obj_x.get('id'))
+        resources[type_x][obj_x['id']] = build_resource_object('name', vlan, obj_x, existing)
+    for obj_x in vlan.get('publicNetworkGateways', {}):
+        existing = resources[type_x].get(obj_x.get('id'))
+        resources[type_x][obj_x['id']] = build_resource_object('name', vlan, obj_x, existing)
 
     return resources
 
-# name_property is what property to use as the name from resource
-# vlan is the vlan object
-# resource has the data we want
-# entry is for any existing data
+
 def build_resource_object(name_property, vlan, resource, entry):
-    new_entry  = {
+    """builds out a resource object and puts the required values in the right place.
+
+    :param: name_property is what property to use as the name from resource
+    :param: vlan is the vlan object
+    :param: resource has the data we want
+    :param: entry is for any existing data
+    """
+    new_entry = {
         'id': resource.get('id'),
         'name': resource.get(name_property),
         'vlan': {vlan.get('networkSpace'): vlan.get('vlanNumber')},
--- docs/cli/reports.rst
+++ docs/cli/reports.rst
@@ -15,3 +15,13 @@ A list of datacenters, and how many servers, VSI, vlans, subnets and public_ips 
 .. click:: SoftLayer.CLI.report.bandwidth:cli
    :prog: report bandwidth
    :show-nested:
+
+
+.. click:: SoftLayer.CLI.report.dc_closures:cli
+   :prog: report datacenter-closures
+   :show-nested:
+
+Displays some basic information about the Servers and other resources that are in Datacenters scheduled to be
+decommissioned in the near future.
+See `IBM Cloud Datacenter Consolidation <https://cloud.ibm.com/docs/get-support?topic=get-support-dc-closure>`_ for
+more information
