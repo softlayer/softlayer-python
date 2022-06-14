d6343257e31a2344d6e16307ced211be253ac77b
caberos <Daniel.Cabero@ibm.com>
2022-06-14 17:04:24 -0400

    add a new command on block object-storage details

--- SoftLayer/CLI/routes.py
+++ SoftLayer/CLI/routes.py
@@ -127,6 +127,7 @@ ALL_ROUTES = [
     ('block:volume-convert', 'SoftLayer.CLI.block.convert:cli'),
     ('block:volume-set-note', 'SoftLayer.CLI.block.set_note:cli'),
     ('block:object-list', 'SoftLayer.CLI.block.object_list:cli'),
+    ('block:object-storage-detail', 'SoftLayer.CLI.block.object_storage_detail:cli'),
 
     ('email', 'SoftLayer.CLI.email'),
     ('email:list', 'SoftLayer.CLI.email.list:cli'),
