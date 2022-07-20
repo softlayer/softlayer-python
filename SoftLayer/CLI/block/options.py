926f481405e6fc5fca6e26f8092e98eb6227c911
caberos <Daniel.Cabero@ibm.com>
2022-07-20 15:32:44 -0400

    block/file volume-options improvements 2

--- SoftLayer/CLI/block/options.py
+++ SoftLayer/CLI/block/options.py
@@ -20,15 +20,27 @@ def cli(env):
     items = order_manager.get_items(PACKAGE_STORAGE)
     datacenters = order_manager.get_regions(PACKAGE_STORAGE)
 
+    network = SoftLayer.NetworkManager(env.client)
+
+    pods = network.get_closed_pods()
+
     iops_table = formatting.Table(['Id', 'Description', 'KeyName'], title='IOPS')
     snapshot_table = formatting.Table(['Id', 'Description', 'KeyName'], title='Snapshot')
     storage_table = formatting.Table(['Id', 'Description', 'KeyName'], title='Storage')
-    datacenter_table = formatting.Table(['Id', 'Description', 'KeyName'], title='Datacenter')
+    datacenter_table = formatting.Table(['Id', 'Description', 'KeyName', 'Notes'], title='Datacenter')
 
     for datacenter in datacenters:
+        closure = []
+        for pod in pods:
+            if datacenter['location']['location']['name'] in str(pod['name']):
+                closure.append(pod['name'])
+
+        notes = '-'
+        if len(closure) > 0:
+            notes = 'closed soon: %s' % (', '.join(closure))
         datacenter_table.add_row([datacenter['location']['locationId'],
                                   datacenter.get('description'),
-                                  datacenter['keyname']])
+                                  datacenter['keyname'], notes])
 
     for item in items:
         if item['itemCategory']['categoryCode'] == 'performance_storage_space':
--- SoftLayer/CLI/file/options.py
+++ SoftLayer/CLI/file/options.py
@@ -21,28 +21,40 @@ def cli(env):
     items = order_manager.get_items(PACKAGE_STORAGE)
     datacenters = order_manager.get_regions(PACKAGE_STORAGE)
 
+    network = SoftLayer.NetworkManager(env.client)
+
+    pods = network.get_closed_pods()
+
     iops_table = formatting.Table(['Id', 'Description', 'KeyName'], title='IOPS')
     snapshot_table = formatting.Table(['Id', 'Description', 'KeyName'], title='Snapshot')
     storage_table = formatting.Table(['Id', 'Description', 'KeyName'], title='Storage')
-    datacenter_table = formatting.Table(['Id', 'Description', 'KeyName'], title='Datacenter')
+    datacenter_table = formatting.Table(['Id', 'Description', 'KeyName', 'Notes'], title='Datacenter')
 
     for datacenter in datacenters:
+        closure = []
+        for pod in pods:
+            if datacenter['location']['location']['name'] in str(pod['name']):
+                closure.append(pod['name'])
+
+        notes = '-'
+        if len(closure) > 0:
+            notes = 'closed soon: %s' % (', '.join(closure))
         datacenter_table.add_row([datacenter['location']['locationId'],
                                   datacenter.get('description'),
-                                  datacenter['keyname']])
+                                  datacenter['keyname'], notes])
 
-    for item in items:
-        if item['itemCategory']['categoryCode'] == 'performance_storage_space':
-            storage_table.add_row([item.get('id'), item.get('description'),
-                                   item.get('keyName')])
+    for item_file in items:
+        if item_file['itemCategory']['categoryCode'] == 'performance_storage_space':
+            storage_table.add_row([item_file.get('id'), item_file.get('description'),
+                                   item_file.get('keyName')])
 
-        if item['itemCategory']['categoryCode'] == 'storage_tier_level':
-            iops_table.add_row([item.get('id'), item.get('description'),
-                                item.get('keyName')])
+        if item_file['itemCategory']['categoryCode'] == 'storage_tier_level':
+            iops_table.add_row([item_file.get('id'), item_file.get('description'),
+                                item_file.get('keyName')])
 
-        if item['itemCategory']['categoryCode'] == 'storage_snapshot_space':
-            snapshot_table.add_row([item.get('id'), item.get('description'),
-                                    item.get('keyName')])
+        if item_file['itemCategory']['categoryCode'] == 'storage_snapshot_space':
+            snapshot_table.add_row([item_file.get('id'), item_file.get('description'),
+                                    item_file.get('keyName')])
 
     env.fout(datacenter_table)
     env.fout(iops_table)
