170fff3308b15c4140ddef2dee6390351a2586a2
caberos <Daniel.Cabero@ibm.com>
2022-10-28 17:16:37 -0400

    fix some problems

--- SoftLayer/CLI/routes.py
+++ SoftLayer/CLI/routes.py
@@ -296,7 +296,7 @@ ALL_ROUTES = [
     ('hardware:monitoring', 'SoftLayer.CLI.hardware.monitoring:cli'),
     ('hardware:notifications', 'SoftLayer.CLI.hardware.notifications:cli'),
     ('hardware:notification-add', 'SoftLayer.CLI.hardware.notification_add:cli'),
-    ('hardware:notification-del', 'SoftLayer.CLI.hardware.notification_delete:cli'),
+    ('hardware:notification-delete', 'SoftLayer.CLI.hardware.notification_delete:cli'),
     ('hardware:create-credential', 'SoftLayer.CLI.hardware.create_credential:cli'),
 
     ('securitygroup', 'SoftLayer.CLI.securitygroup'),
