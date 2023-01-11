9a9b57a7a28ea499ddeaccd28ddf591f2f7919ac
Brian Flores <brian.flores@ibm.com>
2023-01-11 11:22:34 -0400

    updated user remove-access command

--- SoftLayer/CLI/user/remove_access.py
+++ SoftLayer/CLI/user/remove_access.py
@@ -9,12 +9,9 @@ from SoftLayer.CLI import environment
 
 @click.command(cls=SoftLayer.CLI.command.SLCommand, )
 @click.argument('identifier')
-@click.option('--hardware', '-h',
-              help="Display hardware this user has access to.")
-@click.option('--virtual', '-v',
-              help="Display virtual guests this user has access to.")
-@click.option('--dedicated', '-l',
-              help="dedicated host ID ")
+@click.option('--hardware', help="Hardware ID")
+@click.option('--virtual', help="Virtual Guest ID")
+@click.option('--dedicated', help="Dedicated host ID ")
 @environment.pass_env
 def cli(env, identifier, hardware, virtual, dedicated):
     """Remove access from a user to an specific device.
@@ -23,24 +20,24 @@ def cli(env, identifier, hardware, virtual, dedicated):
     """
 
     mgr = SoftLayer.UserManager(env.client)
-    device = ''
     result = False
     if hardware:
-        device = hardware
         result = mgr.remove_hardware_access(identifier, hardware)
+        if result:
+            click.secho("Remove to access to hardware: %s" % hardware, fg='green')
 
     if virtual:
-        device = virtual
         result = mgr.remove_virtual_access(identifier, virtual)
+        if result:
+            click.secho("Remove to access to virtual guest: %s" % virtual, fg='green')
 
     if dedicated:
-        device = dedicated
         result = mgr.remove_dedicated_access(identifier, dedicated)
+        if result:
+            click.secho("Remove to access to dedicated host: %s" % dedicated, fg='green')
 
-    if result:
-        click.secho("Remove to access to device: %s" % device, fg='green')
-    else:
+    if not result:
         raise SoftLayer.exceptions.SoftLayerError('You need argument a hardware, virtual or dedicated identifier.\n'
-                                                  'E.g slcli user 123456 --hardware 91803794\n'
-                                                  '    slcli user 123456 --dedicated 91803793\n'
-                                                  '    slcli user 123456 --virtual 91803792')
+                                                  'E.g slcli user remove-access 123456 --hardware 91803794\n'
+                                                  '    slcli user remove-access 123456 --dedicated 91803793\n'
+                                                  '    slcli user remove-access 123456 --virtual 91803792')
