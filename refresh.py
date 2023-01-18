fff98afb41c1d54d6b5a257f58afc3610a60eed5
ko101 <35339652+ko101@users.noreply.github.com>
2023-01-18 09:47:57 +0530

    Add files via upload

---
+++ refresh.py
@@ -0,0 +1,22 @@
+"""Refresh a dependent duplicate volume with a snapshot from its parent."""
+# :license: MIT, see LICENSE for more details.
+
+import click
+import SoftLayer
+from SoftLayer.CLI import environment
+
+
+@click.command(cls=SoftLayer.CLI.command.SLCommand, )
+@click.argument('volume_id')
+@click.argument('snapshot_id')
+@click.option('--force-refresh', '-f',
+    help="An optional parameter for force refreshing the volume")
+    type=click.Choice(['True', 'False']),
+    default='False',
+@environment.pass_env
+def cli(env, volume_id, snapshot_id, force_refresh):
+    """Refresh a duplicate volume with a snapshot from its parent."""
+    file_manager = SoftLayer.FileStorageManager(env.client)
+    resp = file_manager.refresh_dupe(volume_id, snapshot_id, force_refresh)
+
+    click.echo(resp)
