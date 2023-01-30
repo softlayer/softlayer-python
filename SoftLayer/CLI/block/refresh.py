84c530666873c833d3fc4ca68b8e9906c13660fb
ko101 <35339652+ko101@users.noreply.github.com>
2023-01-30 10:27:39 +0530

    Update refresh.py

--- SoftLayer/CLI/block/refresh.py
+++ SoftLayer/CLI/block/refresh.py
@@ -10,8 +10,7 @@ from SoftLayer.CLI import environment
 @click.argument('volume_id')
 @click.argument('snapshot_id')
 @click.option('--force-refresh', '-f', is_flag = True, default = False, show_default = True,
-              help="Cancel current refresh process and initiates the new refresh."
-              )
+              help="Cancel current refresh process and initiates the new refresh.")
 @environment.pass_env
 def cli(env, volume_id, snapshot_id, force_refresh):
     """Refresh a duplicate volume with a snapshot from its parent."""
