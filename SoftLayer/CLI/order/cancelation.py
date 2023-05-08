bbb60de7590b767b3b11759fd1c1bdff7310990a
caberos <Daniel.Cabero@ibm.com>
2023-05-08 11:58:10 -0400

    fix team code review comments

--- SoftLayer/CLI/order/cancelation.py
+++ SoftLayer/CLI/order/cancelation.py
@@ -6,6 +6,7 @@ from SoftLayer.CLI.command import SLCommand as SLCommand
 from SoftLayer.CLI import environment
 from SoftLayer.CLI import formatting
 from SoftLayer.managers import ordering
+from SoftLayer import utils
 
 
 @click.command(cls=SLCommand)
@@ -14,16 +15,14 @@ def cli(env):
     """List all account cancelations"""
     table = formatting.Table([
         'Case Number', 'Items', 'Created', 'Status', 'Requested by'])
-    table.align['Name'] = 'l'
-    table.align['Package Name'] = 'r'
-    table.align['Package Id'] = 'l'
 
     manager = ordering.OrderingManager(env.client)
     cancelations = manager.get_all_cancelation()
 
     for item in cancelations:
         table.add_row([item.get('ticketId'), item.get('itemCount'),
-                       item.get('createDate'),
+                       utils.clean_time(item.get('createDate'),
+                                        in_format='%Y-%m-%dT%H:%M:%S', out_format='%Y-%m-%d %H:%M'),
                        item['status']['name'],
                        item['user']['firstName'] + ' ' + item['user']['lastName']
                        ])
