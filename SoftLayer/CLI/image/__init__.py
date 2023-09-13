d53029b4f34eb84dc5b4897f8614769f65b113c2
Christopher Gallo <chrisagallo@gmail.com>
2023-09-13 16:14:32 -0500

    #2088 fixed image detail object mask

--- SoftLayer/CLI/image/__init__.py
+++ SoftLayer/CLI/image/__init__.py
@@ -5,9 +5,10 @@ from SoftLayer.CLI import formatting
 
 MASK = ('id,createDate,note,accountId,name,globalIdentifier,parentId,publicFlag,flexImageFlag,'
         'imageType,children[blockDevices[diskImage[softwareReferences[softwareDescription]]]]')
-DETAIL_MASK = MASK + (',firstChild,children[id,blockDevicesDiskSpaceTotal,datacenter,'
-                      'transaction[transactionGroup,transactionStatus],'
-                      'blockDevices[diskImage[capacity,name,units,softwareReferences[softwareDescription]],diskSpace]],'
-                      'note,createDate,status,transaction,accountReferences')
+DETAIL_MASK = ('id,createDate,note,accountId,name,globalIdentifier,parentId,publicFlag,flexImageFlag,'
+        'imageType,firstChild,children[id,blockDevicesDiskSpaceTotal,datacenter,'
+        'transaction[transactionGroup,transactionStatus],'
+        'blockDevices[diskImage[capacity,name,units,softwareReferences[softwareDescription]],diskSpace]],'
+        'status,transaction,accountReferences')
 PUBLIC_TYPE = formatting.FormattedItem('PUBLIC', 'Public')
 PRIVATE_TYPE = formatting.FormattedItem('PRIVATE', 'Private')
