2124d0d1b7a7721ff17f5838ea273d6ff7066fa4
Christopher Gallo <chrisagallo@gmail.com>
2023-09-14 09:08:18 -0500

    tox fixes

--- SoftLayer/CLI/image/__init__.py
+++ SoftLayer/CLI/image/__init__.py
@@ -6,9 +6,9 @@ from SoftLayer.CLI import formatting
 MASK = ('id,createDate,note,accountId,name,globalIdentifier,parentId,publicFlag,flexImageFlag,'
         'imageType,children[blockDevices[diskImage[softwareReferences[softwareDescription]]]]')
 DETAIL_MASK = ('id,createDate,note,accountId,name,globalIdentifier,parentId,publicFlag,flexImageFlag,'
-        'imageType,firstChild,children[id,blockDevicesDiskSpaceTotal,datacenter,'
-        'transaction[transactionGroup,transactionStatus],'
-        'blockDevices[diskImage[capacity,name,units,softwareReferences[softwareDescription]],diskSpace]],'
-        'status,transaction,accountReferences')
+               'imageType,firstChild,children[id,blockDevicesDiskSpaceTotal,datacenter,'
+               'transaction[transactionGroup,transactionStatus],'
+               'blockDevices[diskImage[capacity,name,units,softwareReferences[softwareDescription]],diskSpace]],'
+               'status,transaction,accountReferences')
 PUBLIC_TYPE = formatting.FormattedItem('PUBLIC', 'Public')
 PRIVATE_TYPE = formatting.FormattedItem('PRIVATE', 'Private')
