65dc6dcb264d2330091f67a25cdf49950f52815d
Christopher Gallo <chrisagallo@gmail.com>
2024-05-01 16:56:14 -0500

    v6.2.0 release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='SoftLayer',
-    version='v6.1.11',
+    version='v6.2.0',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/x-rst',
