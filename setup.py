a1844999db12a9558734f075094d2b10b67e1733
Christopher Gallo <chrisagallo@gmail.com>
2024-07-29 17:14:33 -0500

    V6.2.4 version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='SoftLayer',
-    version='v6.2.3',
+    version='v6.2.4',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/x-rst',
